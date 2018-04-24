f5ed59a821cefcf0a626d7603a498c966ecfe58b
hagio <hagio@rrr-z.co.jp>
2018-04-24 21:06:21 +0900

    文字コード判定追加

--- generate.php
+++ generate.php
@@ -26,7 +26,12 @@ $templete = file_get_contents('templates/page.html');
 
 foreach ($file_list as $file_path) {
     $my_text = file_get_contents($file_path);
+
+    if (mb_detect_encoding($my_text, ['UTF-8', 'CP932']) === 'CP932') {
+        $my_text = mb_convert_encoding($my_text, 'UTF-8', 'CP932');
+    }
+
     $my_html = Markdown::defaultTransform($my_text);
-    $body = str_replace('{{ .Content }}', $my_html, $templete);
+    $body = str_replace('{{Content}}', $my_html, $templete);
     file_put_contents($output_directory . DIRECTORY_SEPARATOR . basename($file_path, '.md') . '.html', $body);
 }
