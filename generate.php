cea8ceba5a496e37ddf936209bebfa7d32ea3f7b
Daiki Hagio <hagio@rrr-z.co.jp>
2018-05-03 02:03:49 +0900

    YAML Front-matter対応

--- generate.php
+++ generate.php
@@ -24,14 +24,21 @@ if (!file_exists($output_directory)) {
 
 $templete = file_get_contents('templates/page.html');
 
+$frontMatter = new \Webuni\FrontMatter\FrontMatter();
+
 foreach ($file_list as $file_path) {
     $my_text = file_get_contents($file_path);
 
     if (mb_detect_encoding($my_text, ['UTF-8', 'CP932']) === 'CP932') {
         $my_text = mb_convert_encoding($my_text, 'UTF-8', 'CP932');
     }
+    $doc = $frontMatter->parse($my_text);
 
-    $my_html = Markdown::defaultTransform($my_text);
+    $meta = $doc->getData();
+    $title = isset($meta['title']) ? $meta['title'] : '';
+
+    $my_html = Markdown::defaultTransform($doc->getContent());
     $body = str_replace('{{Content}}', $my_html, $templete);
+    $body = str_replace('{{Title}}', $title, $body);
     file_put_contents($output_directory . DIRECTORY_SEPARATOR . basename($file_path, '.md') . '.html', $body);
 }
